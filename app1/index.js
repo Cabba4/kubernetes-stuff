1b8c3466e4ad0e21dea2df4028557b18eb64c035
cabba04 <master1mist@gmail.com>
2024-07-03 08:45:26 +0300

    app1

---
+++ app1/index.js
@@ -0,0 +1,32 @@
+function createRandomString() {
+  let randomString = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter< 10) {
+    randomString += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return randomString;
+}
+
+const getRandom = () => {
+  const random = createRandomString()
+
+  console.log(new Date().toISOString() + "::" + random);
+
+  setTimeout(getRandom, 5000)
+}
+
+function greetings() {
+  const introText = `
+  Exercise 1.01: Getting started
+
+  Output should be <TimeStamp>::<RandomString>
+  `
+
+  console.log(introText)
+}
+
+greetings()
+getRandom()
