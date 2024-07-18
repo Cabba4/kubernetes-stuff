b86712700b1e7718040e3267662b11b20585cae8
cabba4 <anmolsfs@gmail.com>
2024-07-18 12:50:06 +0300

    exercise 1.11

--- logoutput/pong/index.js
+++ logoutput/pong/index.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const app = express();
+const fs = require('fs')
+const path = require('path')
 const port = process.env.PORT || 3000;
 
 let counter = 0;
 
+const filePath = path.join('/usr/src/app/shared', 'pongCount.txt');
+
 app.get('/pingpong', (req, res) => {
   res.send(`pong ${counter}`);
   counter++;
+  fs.writeFile(filePath, counter.toString(), (err) => {
+    if (err) {
+      console.error('Error writing to file', err);
+    } else {
+      console.log(`Pong counter updated: ${counter}`);
+    }
+  })
 });
 
 app.listen(port, () => {
